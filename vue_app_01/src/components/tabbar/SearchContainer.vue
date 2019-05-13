dfb5f6e03ccb41676e852a6c025c1ae92aaf1761
yanwenjie <790866977@qq.com>
2019-05-13 23:21:10 +0800

    1.0

--- vue_app_01/src/components/tabbar/SearchContainer.vue
+++ vue_app_01/src/components/tabbar/SearchContainer.vue
@@ -1,6 +1,36 @@
 <template>
     <div class="app_search">
-        <h1>搜索</h1>
+        <div class="mui-card">
+			<div class="mui-card-content">
+				<div class="mui-card-content-inner">
+					<input type="text" placeholder="给母亲送最好del礼物">
+                    <mt-button type="danger" size="small">搜索</mt-button>
+				</div>
+			</div>
+		</div>
+        <div class="mui-card">
+            <div class="mui-card-header">搜索结果</div>
+            <div class="mui-card-content">
+                <div class="mui-card-content-inner">
+                    <ul class="mui-table-view">
+                        <li class="mui-table-view-cell mui-media" >
+                            <a href="#">
+                                <img class="mui-media-object mui-pull-left" src="../../img/shuijiao.jpg">
+                                <div class="mui-media-body">
+                                    华为 P30
+                                    <p class='mui-ellipsis'>
+                                        华为手机,爱国者产品
+                                    </p>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+			<div class="mui-card-footer">
+                页脚
+            </div>
+		</div>
     </div>
 </template>
 <script>
---
+++ vxuezi/router/searchlist.js
@@ -0,0 +1,7 @@
+const express=require("express");
+const pool=require("../pool");
+const router=express.Router();
+
+
+
+module.exports=router;
